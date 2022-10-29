2ea256b6e469099828cca77d8d5f7daaf5658fb9
auri222 <auringuyen222@gmail.com>
2022-10-29 19:16:16 +0700

    C2 - add StudentProfile class and main

---
+++ src/exercises/StudentProfile.java
@@ -0,0 +1,14 @@
+package exercises;
+
+public class StudentProfile {
+	private String firstName;
+	private String lastName;
+	private String schoolName;
+	
+	public StudentProfile() {
+		this.firstName = "";
+		this.lastName = "";
+		this.schoolName = "";
+	}
+	
+}
---
+++ src/exercises/StudentProfileMain.java
@@ -0,0 +1,10 @@
+package exercises;
+
+public class StudentProfileMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Student profile main class!");
+	}
+
+}
