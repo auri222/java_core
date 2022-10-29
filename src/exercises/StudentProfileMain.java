e8e736e0ab96bd8622fbbc08221a84d2c2cb1c7c
auri222 <auringuyen222@gmail.com>
2022-10-29 19:22:34 +0700

    C4 - edit StudentProfileMain

--- src/exercises/StudentProfileMain.java
+++ src/exercises/StudentProfileMain.java
@@ -5,6 +5,8 @@ public class StudentProfileMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is Student profile main class!");
+		
+		System.out.println("New string!");
 	}
 
 }
